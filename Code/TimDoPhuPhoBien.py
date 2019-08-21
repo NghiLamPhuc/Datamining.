cd362a6080a289ececb577d2b87285d9e56750a6
lamphucnghi313 <lamphucnghi@gmail.com>
2019-08-22 02:02:18 +0700

    Version1. table lưu có thêm 1 dòng thứ 0 là i1 i2 i3...cột thứ 0 là O1 O2 O3 ....Lưu bằng chuỗi, gây khó khăn lúc duyệt bảng. Sẽ thay đổi table lại thành số trong Version2.

--- Code/TimDoPhuPhoBien.py
+++ Code/TimDoPhuPhoBien.py
@@ -1,80 +1,131 @@
-
+############################################# IMPORT
 from collections import defaultdict
-# import
-######################################
+import pandas as pd
+import os
+############################################# IMPORT
+############################################# INPUT
 #
-# Lấy danh sách các giá trị PHÂN BIỆT.
-#
-def get_unique_item_list(table: dict) -> list:    
-    uniqueItemList = set()
-    for (_, Items) in table.items():
-        uniqueItemList = uniqueItemList.union(set(Items))
-    return sorted(uniqueItemList)
-#
-# Đếm tất cả các giá trị, (để kiểm tra các hàm).
-#
-def count_all_item(table: dict) -> int:
-    count = 0
-    for (_, items) in table.items():
-        count += len(items)
-    return count
-#
-# Đếm tần suất của một giá trị.
-#
-def get_count_one_item_list(items: list, table: dict) -> list:
-    oneItemCountList = dict()
-    for item in items:
-        for (_, itemList) in table.items():
-            if item in itemList:
-                if item not in oneItemCountList:
-                    oneItemCountList[item] = 1
-                else:
-                    oneItemCountList[item] += 1
-    return oneItemCountList
-#
-# Đếm tần suất của một TẬP-x-phần tử giá trị.
-#
+############################################# INPUT
+############################################# HÀM XỬ LÝ INPUT.
+# 1. INPUT THEO KIỂU BẢNG NHỊ PHÂN. (EXCEL).
+# 1
+#def get_input_from_file() -> List[List]:
+# 2. INPUT THEO KIỂU DICT: (ID, TRANSACTIONS). -> CHUYỂN VỀ BẢNG NHỊ PHÂN.
+# 2.1 Hàm lấy dict: (item: tần suất).
+# INPUT: dữ liệu dạng dictionary (id: {item1, item2, ...}).
+# OUTPUT: dictionary (item: frequency).
+def get_unique_item_dict(inputDict: dict) -> dict:
+    uniqueItem = dict()
+    for (_, items) in inputDict.items():
+        for item in items:
+            if item not in uniqueItem:
+                uniqueItem[item] = 1
+            else:
+                uniqueItem[item] += 1
+    return uniqueItem
+# 2.2 Hàm lấy tên các item từ dict: (item: tần suất) và tổng số item.
+# INPUT: dữ liệu dạng dictionary (id: {item1, item2, ...}).
+# OUTPUT: (list các item, tổng số item). 
+def get_item_list_and_count_total(inputDict: dict) -> (list, int):
+    uniqueItem = get_unique_item_dict(inputDict)
+    itemList = list()
+    total = 0
+    for (item, frequency) in uniqueItem.items():
+        itemList.append(item)
+        total += frequency
+    return (sorted(itemList), total)
+# 2.3 Hàm tạo bảng nhị phân từ dict.
+# INPUT: dữ liệu dạng dictionary (id: {item1, item2, ...}).
+# OUTPUT: 2d list, dòng là các id, cột là các item. Xuất hiện là 1, ngược lại 0.
+def create_binary_table(inputDict: dict) -> list:
+    (itemList, _) = get_item_list_and_count_total(inputDict)
+    col = len(itemList)
+    row = len(inputDict)
+    table = [[' 0' for r in range(col + 1)] for c in range(row + 1)]
+    idList = list(inputDict.keys())
 
-#
-# Lọc lại những item xuất hiện >= minsup.
-#
-def get_one_item_list_after_minsup(oneItemCount: dict, minsup: int, totalItem :int) -> list:
-    afterMinsup = list()
-    threshold = round(minsup * totalItem)
+    for item in range(col):
+        table[0][item + 1] = itemList[item]
+    for id in range(row):
+        table[id + 1][0] = idList[id]
+    table[0][0] = '  '
+
+    for indexId in range(len(idList)):
+        itemsAtId = inputDict[idList[indexId]]
+        for indexItem in range(len(itemList)):
+            if itemList[indexItem] in itemsAtId:
+                table[indexId + 1][indexItem + 1] = ' 1'
+    return table
+############################################# HÀM XỬ LÝ INPUT.
+
+############################################# HÀM LẤY TẬP PHỔ BIẾN 1 ITEM.
+# 1.1 Hàm lấy tần suất của 1 item.
+# INPUT: 2d list, dòng là các id, cột là các item. Xuất hiện là 1, ngược lại 0.
+# OUTPUT: dictionary (item: frequency)
+# Hàm này trùng hàm get_unique_item_dict
+#def get_one_item_count(table: list) -> dict:
+#    oneItemCount = dict()
+#    for indexItem in range(1, len(table[0])):
+#        for indexId in range(1, len(table)):
+#            if table[indexId][indexItem] == ' 1':
+#                if table[0][indexItem] not in oneItemCount:
+#                   oneItemCount[table[0][indexItem]] = 1
+#                else:
+#                    oneItemCount[table[0][indexItem]] += 1
+#    return oneItemCount
+# 1.2 Hàm lọc item có tần suất >= minsup.
+def get_one_item_cover(inputDict: dict, minsup: float) -> set:
+    oneItemCount = get_unique_item_dict(inputDict)
+    minOccur = round(minsup * len(oneItemCount))
+    oneCover = list()
     for (item, count) in oneItemCount.items():
-        if count >= threshold:
-            afterMinsup.append(item)
-    return afterMinsup
+        if count >= minOccur:
+            oneCover.append(item)
+    return sorted(oneCover)
+    
+############################################# HÀM LẤY TẬP PHỔ BIẾN 1 PHẦN TỬ.
 
-#
-# Đếm tần suất cặp-2-item.
-#
-def get_count_two_items_list(oneCover: list, table: dict) -> dict:
-    twoCount = defaultdict(dict)
-    numOfOneCover = len(oneCover)
-    for curr in range(numOfOneCover - 1):
-        for next in range(curr + 1, numOfOneCover):
-            for (_, itemList) in table.items():
-                if (oneCover[curr] in itemList) and (oneCover[next] in itemList):
-                    if (oneCover[curr] not in twoCount) or (oneCover[next] not in twoCount[oneCover[curr]]):
-                        twoCount[oneCover[curr]][oneCover[next]] = 1
-                    else:
-                        twoCount[oneCover[curr]][oneCover[next]] += 1
+############################################# HÀM LẤY TẬP PHỔ BIẾN >= 2 PHẦN TỬ.
+# 1. TẠO CÁC TẬP ỨNG VIÊN K PHẦN TỬ, TỪ TẬP PHỦ PHỔ BIẾN K-1 PHẦN TỬ.
+# INPUT: list các Itemset k phần tử.
+# OUTPUT: list các Itemset k+1 phần tử.
+# Duyệt các itemset trong list k phần tử. 0 -> (n - 1)
+# Duyệt các itemset sau đó, cắt itemset sau đó thành list các item. (để ghép với itemset bên trên).
+# Nếu item mới đã có trong itemset trước đó, thì không ghép. Và ngược lại, ghép.
+def generate_next_item_set_from_previous_item_set(prevItemSet: list) -> list:
+    nextItemSet = list()
+    nextItemSetString = list()
+    lenOfNextItemSet = len(prevItemSet[0].split())
+    totalPrevItemSet = len(prevItemSet)
+    for indexPrevSet in range(totalPrevItemSet - 1):
+        for indexNextSet in range(indexPrevSet, totalPrevItemSet):
+            nextItemList = prevItemSet[indexNextSet].split()
+            for indexItem in range(lenOfNextItemSet):
+                if nextItemList[indexItem] not in prevItemSet[indexPrevSet]:
+                    newSetString = prevItemSet[indexPrevSet] + ' ' + nextItemList[indexItem]
+                    if newSetString not in nextItemSetString:
+                        nextItemSetString.append(newSetString)
+    
+    return nextItemSetString
+    
 
-    return twoCount
+# 2. ĐẾM TẦN SUẤT CÁC TẬP K PHẦN TỬ ĐÓ.
+# 3. LOẠI BỎ TẬP CÓ TẦN SUẤT < MINSUP.
+############################################# HÀM LẤY TẬP PHỔ BIẾN >= 2 PHẦN TỬ.
 
 
-minsup = 0.3
-#table = {100: ['A', 'C', 'D', 'I'], 200: ['A', 'C', 'I'], 300: ['C', 'E', 'I'], 400: ['A', 'B', 'D', 'E'], 500: ['B', 'D', 'I'], 600: ['A', 'B', 'D', 'E']}
-table = {'O1': ['i1', 'i7', 'i8'], 'O2': ['i1', 'i2', 'i6', 'i7', 'i8'], 'O3': ['i1', 'i2', 'i6', 'i7'], 'O4': ['i1', 'i8', 'i7'], 'O5': ['i3', 'i4', 'i5', 'i6', 'i8'], 'O6': ['i1', 'i4', 'i5']}
+############################################# HÀM MAIN.
 
-items = get_unique_item_list(table)
-totalItems = len(items)
+def main():
+    minsup = 0.3
+    #inputDict = {100: ['A', 'C', 'D', 'I'], 200: ['A', 'C', 'I'], 300: ['C', 'E', 'I'], 400: ['A', 'B', 'D', 'E'], 500: ['B', 'D', 'I'], 600: ['A', 'B', 'D', 'E']}
+    inputDict = {'O1': ['i1', 'i7', 'i8'], 'O2': ['i1', 'i2', 'i6', 'i7', 'i8'], 'O3': ['i1', 'i2', 'i6', 'i7'], 'O4': ['i1', 'i8', 'i7'], 'O5': ['i3', 'i4', 'i5', 'i6', 'i8'], 'O6': ['i1', 'i4', 'i5']}
 
-oneCount = get_count_one_item_list(items, table)
-oneCover = get_one_item_list_after_minsup(oneCount, minsup, totalItems)
+    table = create_binary_table(inputDict)
+    #print(*table, sep = '\n')
+    #(items, total) = get_item_list_and_count_total(inputDict)
+    oneCover = get_one_item_cover(inputDict, minsup)
+    print(oneCover)
+    
 
-twoCount = get_count_two_items_list(oneCover, table)
-#twoCover = get_item_list_after_minsup(twoCount, minsup, totalItems)
-print(oneCover)
-
+if __name__ == "__main__": main()
