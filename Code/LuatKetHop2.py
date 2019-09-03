b184e671f8dc85ca187234ed57783b01a0bc139d
lamphucnghi313 <lamphucnghi@gmail.com>
2019-09-03 17:32:26 +0700

    Tạm có gì và này nọ.

--- Code/LuatKetHop2.py
+++ Code/LuatKetHop2.py
@@ -31,20 +31,6 @@ def get_sub_list(List: list) -> list:
         allSubList.append(subList)
     return allSubList
 
-# Hàm lấy các luật kết hợp thỏa mãn min_conf.
-def get_associate_rule(inputDict: dict, subList: list, min_conf: float) -> list:
-    associateRules = defaultdict(dict)
-    for eachSub in subList:
-        for nextSub in subList:
-            if not (set(eachSub) & set(nextSub)):
-                conf = check_rule_min_conf(inputDict, eachSub, nextSub, min_conf)
-                if conf > -1.0:
-                    left = ' '.join(eachSub)
-                    right = ' '.join(nextSub)
-                    associateRules[left][right] = conf
-                    
-    return associateRules
-
 # Hàm đếm tần suất của itemList (các item xuất hiện cùng trong 1 id).
 def count_occur_itemList(inputDict: dict, itemList: list) -> int:
     count = 0
@@ -64,17 +50,43 @@ def check_rule_min_conf(inputDict: dict, left: list, right: list, min_conf: floa
     
     return conf if (conf >= min_conf) else -1.0
 
+# Hàm lấy các luật kết hợp TỪ MỘT TẬP (muốn lấy hết thì for) thỏa mãn min_conf. 
+def get_associate_rule(inputDict: dict, subList: list, min_conf: float) -> list:
+    associateRules = defaultdict(dict)
+    for eachSub in subList:
+        for nextSub in subList:
+            if not (set(eachSub) & set(nextSub)):
+                conf = check_rule_min_conf(inputDict, eachSub, nextSub, min_conf)
+                if conf > -1.0:
+                    left = ' '.join(eachSub)
+                    right = ' '.join(nextSub)
+                    associateRules[left][right] = conf
+                    
+    return associateRules
+
+# Hàm lấy tất cả các strong rules.
+def get_all_strong_rule(inputDict: dict, frequentItemSet: list, min_conf: float) -> list:
+    ruleList = list()
+    for iset in frequentItemSet:
+        subList = get_sub_list(iset)
+        rules = get_associate_rule(inputDict, subList, min_conf)
+        for (left, rights) in rules.items():
+            for (right, conf) in rights.items():
+                ruleList.append('[' + left + ']' + '->' + '[' + right + ']' + ': ' + str(conf) )
+    return ruleList
+
 def main():
     start = datetime.now()
 
-    min_conf = 1.0
-    #frequentItemSet = [['i1', 'i7'],['i6', 'i8'],['i1', 'i2', 'i6'],['i1', 'i7', 'i8']]
-    frequentItemSet = [['B', 'H'], ['A', 'H', 'M', 'N']]
+    min_conf = 0.5
+    frequentItemSet = [['i1', 'i7'],['i6', 'i8'],['i1', 'i2', 'i6'],['i1', 'i7', 'i8']]
+    #frequentItemSet = [['B', 'H'], ['A', 'H', 'M', 'N']]
     name = ['baitapbuoi2.txt', 'baitapthem7.txt']
-    inputDict = read_input_file(link_input, name[-1])
-    #[print(*get_sub_list(frequentItemSet[index]), sep = '\n') for index in range(len(frequentItemSet))]
+    inputDict = read_input_file(link_input, name[0])
     
-    print(get_associate_rule(inputDict, get_sub_list(frequentItemSet[0]), min_conf))
+    
+    ruleList = get_all_strong_rule(inputDict, frequentItemSet, min_conf)
+    print(*ruleList, sep = '\n')
 
     print (datetime.now()-start)
     
