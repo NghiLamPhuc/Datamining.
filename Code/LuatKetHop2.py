49c8d250502f21149438fd82e4817bee8ee1b310
lamphucnghi313 <lamphucnghi@gmail.com>
2019-09-05 00:06:52 +0700

    Bỏ được các tập con trùng lặp.

--- Code/LuatKetHop2.py
+++ Code/LuatKetHop2.py
@@ -49,6 +49,7 @@ def get_sub_list(List: list) -> list:
             if ( i & (1 << j) ) > 0:
                 subList.append(List[j])
         allSubList.append(subList)
+    
     return allSubList
 
 # Hàm đếm tần suất của itemList (các item xuất hiện cùng trong 1 id).
@@ -62,24 +63,24 @@ def count_occur_itemList(inputDict: dict, itemList: list) -> int:
 # Đếm tần suất các item trong left, right xuất hiện cùng nhau.
 # Chia cho tần suất các item trong left xuất hiện cùng nhau.
 # >= min_conf thì trả về tỉ lệ, ngược lại -1.
-def check_rule_min_conf(inputDict: dict, left: list, right: list, min_conf: float) -> float:
+def calc_rule_conf(inputDict: dict, left: list, right: list) -> float:
     countLeft = count_occur_itemList(inputDict, left)
     rules = list(set(left) ^ set(right))
     countRule = count_occur_itemList(inputDict, rules)
     conf = countRule / countLeft
     
-    return conf if (conf >= min_conf) else -1.0
+    return conf
 
-# Hàm lấy các luật kết hợp TỪ MỘT TẬP (muốn lấy hết thì for) thỏa mãn min_conf. 
+# Hàm lấy các luật kết hợp TỪ MỘT TẬP (muốn lấy hết thì for) thỏa mãn min_conf.
 def get_associate_rule(inputDict: dict, subList: list, min_conf: float) -> list:
     associateRules = defaultdict(dict)
     for eachSub in subList:
         for nextSub in subList:
             if not (set(eachSub) & set(nextSub)):
-                conf = check_rule_min_conf(inputDict, eachSub, nextSub, min_conf)
-                if conf > -1.0:
-                    left = ' '.join(eachSub)
-                    right = ' '.join(nextSub)
+                conf = calc_rule_conf(inputDict, eachSub, nextSub)
+                if conf >= min_conf:
+                    left = ','.join(eachSub)
+                    right = ','.join(nextSub)
                     associateRules[left][right] = conf
                     
     return associateRules
@@ -87,12 +88,18 @@ def get_associate_rule(inputDict: dict, subList: list, min_conf: float) -> list:
 # Hàm lấy tất cả các strong rules.
 def get_all_strong_rule(inputDict: dict, frequentItemSet: list, min_conf: float) -> list:
     ruleList = list()
+    subList = list()
     for iset in frequentItemSet:
-        subList = get_sub_list(iset)
-        rules = get_associate_rule(inputDict, subList, min_conf)
-        for (left, rights) in rules.items():
-            for (right, conf) in rights.items():
-                ruleList.append('[' + left + ']' + '->' + '[' + right + ']' + ': ' + str(conf) )
+        allSubList = get_sub_list(iset) # bị trùng lại các tập con. Cần loại trước khi chạy luật.
+        for sL in allSubList:
+            if sL not in subList:
+                subList.append(sL)
+    
+    rules = get_associate_rule(inputDict, subList, min_conf)
+    
+    for (left, rights) in rules.items():
+        for (right, conf) in rights.items():
+            ruleList.append('[' + left + ']' + '->' + '[' + right + ']' + ': ' + str(conf) )
     return ruleList
 
 def main():
@@ -101,14 +108,16 @@ def main():
     min_conf = 1.0
     #frequentItemSet = [['i1', 'i7'],['i6', 'i8'],['i1', 'i2', 'i6'],['i1', 'i7', 'i8']]
     #frequentItemSet = [['B', 'H'], ['A', 'H', 'M', 'N']]
-    frequentItemSet = [['i2', 'i3', 'i5'], ['i2', 'i3', 'i8']]
-    name = ['baitapbuoi2.txt', 'baitapthem7.txt', 'baitapbuoi22.txt']
+    #frequentItemSet = [['i2', 'i3', 'i5'], ['i2', 'i3', 'i8']]
+    frequentItemSet = [['i3', 'i5', 'i7'], ['i3', 'i4', 'i5', 'i6']]
+
+    name = ['baitapbuoi2.txt', 'baitapthem7.txt', 'baitapbuoi22.txt', 'baitapbuoi23.txt']
     inputDict = read_input_file(link_input, name[-1])
     
     ruleList = get_all_strong_rule(inputDict, frequentItemSet, min_conf)
-    list_to_txt(ruleList, link_folder + 'Strong_Rule\\', 'Rule_' + name[-1])
+    list_to_txt(ruleList, link_folder + 'Strong_Rule\\', '2Rule_' + name[-1])
     #print(*ruleList, sep = '\n')
 
-    print (datetime.now()-start)
+    print (datetime.now() - start)
     
 if __name__ == "__main__": main()
