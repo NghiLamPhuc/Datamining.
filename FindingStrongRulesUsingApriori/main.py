d081bf08dc9c60d3d015d5d960cf2830c3e99715
lamphucnghi313 <lamphucnghi@gmail.com>
2019-09-22 14:13:52 +0700

    Thêm ghi file topten thôi. Éc Éc.

--- FindingStrongRulesUsingApriori/main.py
+++ FindingStrongRulesUsingApriori/main.py
@@ -25,17 +25,19 @@ def get_possile_itemset(inputDict: dict, minsup: float, nameFile) -> int:
         return 0
 
 def get_SR(inputDict: dict, FISDir, FISName, minconf: float, minsupp: float):
-    # lấy số minsup từ FISDir
-    # FISDirList = list(FISDir)
-    # minsupStr = FISDirList[-2:-4]
     frequentItemSet = read_file.read_lines_to_list(FISDir, FISName, ', ')
-    ruleList = Strong_rules.get_all_strong_rule(inputDict, frequentItemSet, minconf)
+    (ruleList, topTenRules) = Strong_rules.get_all_strong_rule(inputDict, frequentItemSet, minconf)
     write_file.list_to_txt(ruleList, './RESULT/Strong_rule_minsupp_' + str(minsupp) + ' minconf_' + str(minconf) + '/', 'Rule_' + FISName) if ruleList else print('Không có luật thỏa mãn.')
+    write_file.list_to_txt(topTenRules, './RESULT/Topten_Strong_rule_minsupp_' + str(minsupp) + ' minconf_' + str(minconf) + '/', '10_Rules_' + FISName) if topTenRules else print('Không có luật thỏa mãn.')
 
 def run(inputDict: dict, minsupp: float, minconf: float, nameFile):
     ## Chay buoc 1.
     itemset = get_possile_itemset(inputDict, minsupp, nameFile)
-    freqitemset = get_FIS(inputDict, minsupp, nameFile)
+    freqitemset = list()
+    if itemset == 1:
+        freqitemset = get_FIS(inputDict, minsupp, nameFile)
+    else:
+        print('Không có tập phủ phổ biến!')
     
     ## Chay buoc 2.
     if freqitemset == 1:
@@ -53,18 +55,21 @@ def main():
     
     inputDir = './input/'
     # inputDir = './Generate Input/'
-    
+    # print(Frequent_Itemset.get_total_items(read_file.read_input_file(inputDir, 'plants.txt', splitType[2]))) # Tổng số item trong input.
     nameFileList = sorted(os.listdir(inputDir))
     splitType = [' ', ', ', ',']
+    # minsupp = 0.3
+    # minconf = 0.9
+    
     minsupp = 0.1
     minconf = 0.85
-    
-    ## chạy 7 file input đầu, trừ plants.txt
+
+    # chạy 7 file input đầu, trừ plants.txt
     # for indexInput in range(len(nameFileList) - 1):
     #     nameFile = nameFileList[indexInput]
     #     print(nameFile)
         
-    #     # inputDict = read_file.read_input_file(inputDir, nameFile, splitType[1]) # input khong co te^n transaction.
+    #     # inputDict = read_file.read_input_file(inputDir, nameFile, splitType[1]) # input khong co te^n transaction, khi đó, trong Strong_rule đổi hàm count_occur_itemlist.
     #     inputDict = read_file.read_input_file_with_row_name(inputDir, nameFile, splitType[1])
     #     run(inputDict, minsupp, minconf, nameFile)
     
@@ -73,9 +78,13 @@ def main():
     nameFile = nameFileList[indexInput]
     print(nameFile)
 
-    inputDict = read_file.read_input_file_with_row_name(inputDir, nameFile, splitType[2])
+    inputDict = read_file.read_input_file_with_row_name(inputDir, nameFile, splitType[1])
     run(inputDict, minsupp, minconf, nameFile)
-    
+    # minsup = 0.1
+    # while (minsup < 0.9):
+    #     run(inputDict, minsup, minconf, nameFile)
+    #     minsup += 0.1
+    #     print(minsup)
 
     
     print(datetime.now() - start)
